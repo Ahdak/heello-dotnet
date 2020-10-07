7b92c2d8c1c51138bbb67bdd233d32d3533c4a18
Ahmed DAMMAK <ahmed.dammak.net@gmail.com>
2020-10-07 22:49:38 +0200

    Init C# project - End part 1 - Introducing C# and .Net

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Console.WriteLine("Empty parameters") ;
+            } else {
+                Console.WriteLine($"Hello {args[0]} !");
+            }
+            
+        }
+    }
+}
